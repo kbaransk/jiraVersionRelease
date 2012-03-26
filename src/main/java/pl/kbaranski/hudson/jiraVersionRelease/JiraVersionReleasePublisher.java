cdb7118bc6bb8b7e81f0bd718d229e1acaa2e421
Krzysztof Barański <kbaransk@gmail.com>
2012-03-26 23:28:19 +0200

    - JIRA instances are configurable at global level. During configuration of job you should only choose appropriate instance from list of previously defined instances. - Missing help pages were added to project (currently this are stubs only)

--- src/main/java/pl/kbaranski/hudson/jiraVersionRelease/JiraVersionReleasePublisher.java
+++ src/main/java/pl/kbaranski/hudson/jiraVersionRelease/JiraVersionReleasePublisher.java
@@ -2,23 +2,26 @@ package pl.kbaranski.hudson.jiraVersionRelease;
 
 import hudson.Extension;
 import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.JobProperty;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
-import hudson.model.Build;
-import hudson.model.BuildListener;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
-import hudson.tasks.Builder;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
+import hudson.util.CopyOnWriteList;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Calendar;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.xml.rpc.ServiceException;
+
+import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
@@ -30,60 +33,38 @@ import pl.kbaranski.hudson.jiraVersionRelease.soap.RemoteAuthenticationException
 import pl.kbaranski.hudson.jiraVersionRelease.soap.RemoteException;
 import pl.kbaranski.hudson.jiraVersionRelease.soap.RemoteVersion;
 
-/**
- * Sample {@link Builder}.
- *
- * <p>
- * When the user configures the project and enables this builder,
- * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
- * and a new {@link JiraVersionReleasePublisher} is created. The created
- * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #name})
- * to remember the configuration.
- *
- * <p>
- * When a build is performed, the {@link #perform(Build, Launcher, BuildListener)} method
- * will be invoked. 
- *
- * @author Kohsuke Kawaguchi
- */
 public class JiraVersionReleasePublisher extends Notifier {
 
-    private String url;
-    private String user;
-    private String pass;
-    
-    private String projectKey;
-    
-    private String prefixRegexp;
-    
+	private String instanceName;
+
+	private String projectKey;
+
+	private String prefixRegexp;
+
 	public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
 
+	private final static Logger LOG = Logger.getLogger(JiraVersionReleasePublisher.class.getName());
+
 	@DataBoundConstructor
-	public JiraVersionReleasePublisher(String url, String user, String pass, String projectKey, String prefixRegexp) {
-		this.url = url;
-		this.user = user;
-		this.pass = pass;
+	public JiraVersionReleasePublisher(String instanceName,
+			String projectKey, String prefixRegexp) {
+		this.instanceName = instanceName;
 		this.projectKey = projectKey;
 		this.prefixRegexp = prefixRegexp;
 	}
-	
- 	public String getUrl() {
-		return url;
+
+	public String getInstanceName() {
+		return instanceName;
 	}
 
-	public String getUser() {
-		return user;
+	public void setInstanceName(String instanceName) {
+		this.instanceName = instanceName;
 	}
 
-	public String getPass() {
-		return pass;
-	}
-	
 	public String getProjectKey() {
 		return projectKey;
 	}
-	
+
 	public String getPrefixRegexp() {
 		return prefixRegexp;
 	}
@@ -93,108 +74,165 @@ public class JiraVersionReleasePublisher extends Notifier {
 		return true;
 	}
 
-    public BuildStepMonitor getRequiredMonitorService() {
-        return BuildStepMonitor.BUILD;
-    }
+	public BuildStepMonitor getRequiredMonitorService() {
+		return BuildStepMonitor.BUILD;
+	}
+
+	public TrackerInstance getCurrentTracker() {
+		if (this.instanceName == null)
+			return null;
+		for (TrackerInstance ti : DESCRIPTOR.getInstances()) {
+			if (instanceName.equals(ti.getInstanceName())) {
+				return ti;
+			}
+		}
+		return null;
+	}
 
 	@Override
-    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
-    	JiraSoapServiceService serviceLocator = new JiraSoapServiceServiceLocator();
+	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher,
+			BuildListener listener) {
+		JiraSoapServiceService serviceLocator = new JiraSoapServiceServiceLocator();
 
-    	try {
-    		//	Wzorce itp wykorzystywane przy sprawdzaniu wersji
+		try {
+			// Wzorce itp wykorzystywane przy sprawdzaniu wersji
 			Pattern versionNumberPrefixPattern = Pattern.compile(prefixRegexp);
 			String versionNumberFullRegex = prefixRegexp + build.getNumber();
-			String newVerName = null;			
+			String newVerName = null;
 
-    		//	Podłączenie do JIRA + logowanie
-			JiraSoapService soapService = serviceLocator.getJirasoapserviceV2(new URL(this.url));
-			String soapToken = soapService.login(this.user, this.pass);
-			
-			//	Pobranie zdalnych wersji i ich sprawdzanie
-			RemoteVersion[] versions = soapService.getVersions(soapToken, this.projectKey);
+			// Podłączenie do JIRA + logowanie
+			TrackerInstance trackerInstance = getCurrentTracker();
+			JiraSoapService soapService = serviceLocator
+					.getJirasoapserviceV2(trackerInstance.getUrl());
+			String soapToken = soapService.login(trackerInstance.getUser(), trackerInstance.getPass());
+
+			// Pobranie zdalnych wersji i ich sprawdzanie
+			RemoteVersion[] versions = soapService.getVersions(soapToken,
+					this.projectKey);
 			for (RemoteVersion version : versions) {
-				//	Z wersjami wydanymi nic nie chcemy robic
+				// Z wersjami wydanymi nic nie chcemy robic
 				if (version.isReleased() || version.isArchived()) {
 					continue;
 				}
-				//	Pomijamy również wersje nie pasujące do wzorca
+				// Pomijamy również wersje nie pasujące do wzorca
 				if (!Pattern.matches(versionNumberFullRegex, version.getName())) {
-//					System.out.println(version.getName() + " nie pasuje do wzorca: " + versionNumberFullRegex);
+					// System.out.println(version.getName() +
+					// " nie pasuje do wzorca: " + versionNumberFullRegex);
 					continue;
 				}
-				//	Teraz wiemy już, że znaleziona wersja, to ta odpowiadająca poprzednio zbudowanej wersji w Hudson
-				Matcher matcher = versionNumberPrefixPattern.matcher(version.getName());
+				// Teraz wiemy już, że znaleziona wersja, to ta odpowiadająca
+				// poprzednio zbudowanej wersji w Hudson
+				Matcher matcher = versionNumberPrefixPattern.matcher(version
+						.getName());
 				if (matcher.find()) {
 					newVerName = matcher.group();
 					version.setReleased(true);
 					version.setReleaseDate(Calendar.getInstance());
 					soapService.releaseVersion(soapToken, projectKey, version);
-					listener.getLogger().println("JIRA: Wydano wersje " + version.getName() + " w projekcie " + this.getProjectKey());
+					listener.getLogger().println(
+							"JIRA: Wydano wersje " + version.getName()
+									+ " w projekcie " + this.getProjectKey());
 					break;
 				}
 			}
-			
+
 			if (newVerName != null) {
 				// tworzymy nową wersję
-				String newVersionFullName = newVerName + (build.getNumber() + 1);
+				String newVersionFullName = newVerName
+						+ (build.getNumber() + 1);
 				RemoteVersion newVer = new RemoteVersion();
 				newVer.setName(newVersionFullName);
 				soapService.addVersion(soapToken, projectKey, newVer);
-				listener.getLogger().println("JIRA: Utworzono wersje " + newVersionFullName + " w projekcie " + this.getProjectKey());
+				listener.getLogger().println(
+						"JIRA: Utworzono wersje " + newVersionFullName
+								+ " w projekcie " + this.getProjectKey());
 				return true;
-			}
-			else {
-				listener.getLogger().println("JIRA: W JIRA nie odnaleziono wersji odpowiadającej biezacemu numerowi kopilacji");
+			} else {
+				listener.getLogger()
+						.println(
+								"JIRA: W JIRA nie odnaleziono wersji odpowiadającej biezacemu numerowi kopilacji");
 			}
 			soapService.logout(soapToken);
-		} catch (MalformedURLException e) {
-			System.out.println(new StringBuilder().append("[").append(e.getClass().getSimpleName()).append("] ").append(e.getMessage()).append(": \n").append(e.getStackTrace()).toString());
 		} catch (ServiceException e) {
-			System.out.println(new StringBuilder().append("[").append(e.getClass().getSimpleName()).append("] ").append(e.getMessage()).append(": \n").append(e.getStackTrace()).toString());
+			System.out.println(new StringBuilder().append("[")
+					.append(e.getClass().getSimpleName()).append("] ")
+					.append(e.getMessage()).append(": \n")
+					.append(e.getStackTrace()).toString());
 		} catch (RemoteAuthenticationException e) {
-			listener.getLogger().println("JIRA: Nie uwierzytelniono uzytkownika");
-			System.out.println(new StringBuilder().append("[").append(e.getClass().getSimpleName()).append("] ").append(e.getMessage()).append(": \n").append(e.getStackTrace()).toString());
+			listener.getLogger().println(
+					"JIRA: Nie uwierzytelniono uzytkownika");
+			System.out.println(new StringBuilder().append("[")
+					.append(e.getClass().getSimpleName()).append("] ")
+					.append(e.getMessage()).append(": \n")
+					.append(e.getStackTrace()).toString());
 		} catch (RemoteException e) {
-			System.out.println(new StringBuilder().append("[").append(e.getClass().getSimpleName()).append("] ").append(e.getMessage()).append(": \n").append(e.getStackTrace()).toString());
+			System.out.println(new StringBuilder().append("[")
+					.append(e.getClass().getSimpleName()).append("] ")
+					.append(e.getMessage()).append(": \n")
+					.append(e.getStackTrace()).toString());
 		} catch (java.rmi.RemoteException e) {
-			System.out.println(new StringBuilder().append("[").append(e.getClass().getSimpleName()).append("] ").append(e.getMessage()).append(": \n").append(e.getStackTrace()).toString());
+			System.out.println(new StringBuilder().append("[")
+					.append(e.getClass().getSimpleName()).append("] ")
+					.append(e.getMessage()).append(": \n")
+					.append(e.getStackTrace()).toString());
 		} catch (Exception e) {
-			System.out.println(new StringBuilder().append("[").append(e.getClass().getSimpleName()).append("] ").append(e.getMessage()).append(": \n").append(e.getStackTrace()).toString());
+			System.out.println(new StringBuilder().append("[")
+					.append(e.getClass().getSimpleName()).append("] ")
+					.append(e.getMessage()).append(": \n")
+					.append(e.getStackTrace()).toString());
 		}
-        return false;
-    }
-    
-    /**
-     * Descriptor for {@link JiraVersionReleasePublisher}. Used as a singleton.
-     * The class is marked as public so that it can be accessed from views.
-     *
-     * <p>
-     * See <tt>views/hudson/plugins/hello_world/JiraVersionReleasePublisher/*.jelly</tt>
-     * for the actual HTML fragment for the configuration screen.
-     */
-    @Extension
-    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+		return false;
+	}
 
-        public DescriptorImpl() {
-            super(JiraVersionReleasePublisher.class);
-        }
+	@Override
+	public DescriptorImpl getDescriptor() {
+		return (DescriptorImpl) super.getDescriptor();
+	}
 
-        @Override
-        public String getDisplayName() {
-            return "JIRA Version Releaser";
-        }
-        
-        @Override
-        public String getHelpFile() {
-        	return "/plugin/jiraVersionRelease/help-config.html";
-        }
+	/**
+	 * Descriptor for {@link JiraVersionReleasePublisher}. Used as a singleton.
+	 * The class is marked as public so that it can be accessed from views.
+	 *
+	 * <p>
+	 * See
+	 * <tt>views/hudson/plugins/hello_world/JiraVersionReleasePublisher/*.jelly</tt>
+	 * for the actual HTML fragment for the configuration screen.
+	 */
+	@Extension
+	public static final class DescriptorImpl extends
+			BuildStepDescriptor<Publisher> {
 
-		@SuppressWarnings("unchecked")
+		private final CopyOnWriteList<TrackerInstance> instances = new CopyOnWriteList<TrackerInstance>();
+
+
+		public DescriptorImpl() {
+			super(JiraVersionReleasePublisher.class);
+			load();
+		}
+
 		@Override
-		public boolean isApplicable(Class<? extends AbstractProject> arg0) {
-			return true;
+		public String getDisplayName() {
+			return "JIRA Version Releaser";
 		}
-    }
-}
 
+		@Override
+		public String getHelpFile() {
+			LOG.info("Domyślny helpFile : " + super.getHelpFile());
+			LOG.info("Id : " + getId());
+			return "/plugin/jiraVersionRelease/help-config.html";
+		}
+
+		@Override
+		public boolean isApplicable(@SuppressWarnings("rawtypes") Class<? extends AbstractProject> jobType) {
+			return AbstractProject.class.isAssignableFrom(jobType);
+		}
+
+		public void setInstances(TrackerInstance instance) {
+			instances.add(instance);
+		}
+
+		public TrackerInstance[] getInstances() {
+			return instances.toArray(new TrackerInstance[instances.size()]);
+		}
+	}
+}
---
+++ src/main/java/pl/kbaranski/hudson/jiraVersionRelease/TrackerInstance.java
@@ -0,0 +1,56 @@
+package pl.kbaranski.hudson.jiraVersionRelease;
+
+import java.net.URL;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+
+/**
+ * This class describes single instance of JIRA issue tracker.
+ * It's quite probable that single instance of JIRA will contains projects connected with mamy different Jenkins / Hudson jobs so it's good idea to setup this data only once.
+ * @author Krzysztof Barański
+ * @since 1.1
+ */
+public class TrackerInstance {
+
+	/** Name of JIRA instance. This values will be presented in select box on job setup page. */
+	private String instanceName;
+	/** URL of JIRA ionstance. */
+	private URL url;
+	/** Name of the user that will be used to manage versions in JIRA. */
+	private String user;
+	/** Password to the account of the user specified in {@code user}. */
+	private String pass;
+
+	@DataBoundConstructor
+	public TrackerInstance(String instanceName, URL url, String user, String pass) {
+		this.instanceName = instanceName;
+		this.url = url;
+		this.user = user;
+		this.pass = pass;
+	}
+	public String getInstanceName() {
+		return instanceName;
+	}
+	public void setInstanceName(String instanceName) {
+		this.instanceName = instanceName;
+	}
+	public URL getUrl() {
+		return url;
+	}
+	public void setUrl(URL url) {
+		this.url = url;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+}
